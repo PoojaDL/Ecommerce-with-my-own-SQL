89814f21d7fa25be353c8a4e838e88048e4ee83c
PoojaDL <poojadl2002@gmail.com>
2023-11-17 15:09:25 +0530

    used mysql to store data

---
+++ models/product.js
@@ -0,0 +1,32 @@
+const db = require("../util/database");
+
+const Cart = require("./cart");
+
+module.exports = class Product {
+  constructor(id, title, imageUrl, description, price) {
+    this.id = id;
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    return db.execute(
+      "Insert into products (title,price,imageUrl,description) values (?,?,?,?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
+  }
+
+  static deleteById(id) {
+    return db.execute(`delete from products where id=${id}`);
+  }
+
+  static fetchAll() {
+    return db.execute("select * from products");
+  }
+
+  static findById(id) {
+    return db.execute(`select * from products where id=${id}`);
+  }
+};
